f623d6b3d4407ae2728d29c4426bfbaf3d312e7a
alleytopcat <donmaoccio@icloud.com>
2021-09-03 23:20:17 -0500

    update

---
+++ master-python/02-variables-y-tipos/tipos.py
@@ -0,0 +1,27 @@
+nada = None
+string = "Hola prueba de String"
+integer = 80
+float = 6.69
+boolean = True #puede ser false
+array = [10,20,30,100,200] #lista/arrays de datos
+stringlist = [44,"Cuarenta y cuatro",30,"Treinta"] #Coleccion de datos
+tupla = ("master","en", "python") #las tuplas no cambian
+dictionary = {
+    "nombre": "Mario",
+    "apellido": "Benítez",
+    "cursos": "Master en python"
+}
+
+rango = range(9)
+dato_bytes = b"prueba"
+
+#imprimir variable
+print(rango)
+#visualizar tipo de dato
+print(type(rango))
+
+
+#Convertir Datos
+text = "Hola texto prueba"
+num = str(232)
+print(f"{text} {num}")
--- master-python/02-variables-y-tipos/variables.py
+++ master-python/02-variables-y-tipos/variables.py
@@ -22,3 +22,4 @@ web = "alleytopcat"
 print(nombre + " " + apellido + " - " + web)
 print(f"{nombre} {apellido} - {web}")
 print("Hola me llamo {} {} y mi web es: {}".format(nombre, apellido, web))
+
